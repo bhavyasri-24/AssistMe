d6a295b42b211bf5afb54223e6c0412fe522cd67
bhavyasri-24 <bhavyakureti@gmail.com>
2026-04-17 21:27:21 +0530

    add analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 import AuthProvider from "./context/AuthContext";
 
+import { Analytics } from "@vercel/analytics/react";
+
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <App />
+    <Analytics />
   </AuthProvider>,
 );
